692b8a9f6e84d00d4c1fa9c58dc4fcdbc15a651d
Bram <bram.boogaarts@gmail.com>
2021-11-09 19:01:20 +0100

    Finished HTML/JS miniopdrachten

--- Dag 1/miniopdrachten.js
+++ Dag 1/miniopdrachten.js
@@ -8,6 +8,7 @@ function opdracht1(inputId) {
     }
 }
 
+
 function opdracht2_3(inputId1, inputId2) {
     var woord = document.getElementById(inputId1).value.toLowerCase();
     var letter = document.getElementById(inputId2).value.toLowerCase();
@@ -35,6 +36,9 @@ function opdracht2_3(inputId1, inputId2) {
         }
     }
 }
+
+
+var buttonClicks = {A:0, B:0, C:0, D:0};
 
 function opdracht4_5(ev) {
     if(ev.target.id == "A") {
@@ -64,4 +68,49 @@ function opdracht4_5(ev) {
     document.getElementById("div1").innerHTML += ("U heeft op knop " + ev.target.id + " geklikt." + '<br>');
 }
 
-var buttonClicks = {A:0, B:0, C:0, D:0};
+
+var seasonsImgs = ["https://www.hdwallpapers.in/download/moraine_lake_in_winter_canada-1920x1200.jpg",
+                   "http://getwallpapers.com/wallpaper/full/f/6/3/1021023-best-spring-meadow-wallpaper-1920x1200.jpg",
+                   "https://www.achensee.com/wp-content/uploads/2019/07/Indian_Summer_am_Achensee-Seeberg%C2%A9AchenseeTourismus.jpg",
+                   "https://www.desicomments.com/wp-content/uploads/2017/02/Nice-Image-Of-Autumn.jpg"];
+var seasonCountA = 0;
+var seasonCountB = 0;
+
+function opdracht6_7(ev) {
+    if(ev.target.id == "buttonA") {
+        seasonCountA += 1;
+        let seasonRem = seasonCountA % 4;
+        document.getElementById("seasonImgA").src = seasonsImgs[seasonRem];
+        document.getElementById("spanA").innerHTML = parseInt(seasonCountA / 4);
+    }
+    else if(ev.target.id == "buttonB") {
+        seasonCountB += 1;
+        let seasonRem = seasonCountB % 4;
+        document.getElementById("seasonImgB").src = seasonsImgs[seasonRem];
+        document.getElementById("spanB").innerHTML = parseInt(seasonCountB / 4);
+    }
+    else if(ev.target.id == "buttonC") {
+        if(document.getElementById("seasonImgA").src == document.getElementById("seasonImgB").src) {
+            console.log("HETZELFDE");
+        }
+        else {
+            console.log("NIET HETZELFDE");
+        }
+    }
+}
+
+
+var seasonCountC = 0;
+
+function opdracht8() {
+    seasonCountC += 1;
+    let seasonRem = seasonCountC % 4;
+    document.getElementById("seasonImgC").src = seasonsImgs[seasonRem];
+
+    if (seasonRem == 1) {
+        document.getElementById("imgLambs").hidden = false;
+    }
+    else {
+        document.getElementById("imgLambs").hidden = true;
+    }
+}
